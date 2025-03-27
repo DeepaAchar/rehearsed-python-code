af1552ca62efa2d36764ae1e6b9a1120210211ef
Deepa <deepav2008@gmail.com>
2025-03-27 18:45:01 +1300

    Add files via upload
    Exceptions corrected!

--- fuel.py
+++ fuel.py
@@ -1,28 +1,49 @@
+""" 
+In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, 
+wherein each of X and Y is an integer, and then outputs, as a percentage rounded to the nearest integer, 
+how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. 
+And if 99% or more remains, output F instead to indicate that the tank is essentially full.
+
+If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. 
+(It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
+ """
 # https://docs.python.org/3/tutorial/errors.html
+
+import sys
 
 def fuel_monitor():
     while True:
-        prompt = input("Enter the fraction (format: x/y): ")
-        x, y = prompt.split('/')
-        try:
+        try: 
+            prompt = input("Enter the fraction (format: x/y): ")
+            x, y = prompt.split('/')
+
             x = int(x)
-            y = int(y) 
+            y = int(y)
+                             
+            if y == 0:
+                raise ZeroDivisionError 
+            elif x > y:
+                raise ValueError 
+            else:
+                tank =  x / y * 100
+                # print(f'z: {tank}')
+                if tank <= 1:
+                    return 'E'
+                elif tank >=99:
+                    return 'F'
+                else:
+                    return str(f'Fuel Available: {tank: .2f}') + '%'
 
-            if x <= y:
-                break
+                
+        except ValueError:
+           print("Raise: ValueError")
+           pass
 
-        except (ValueError, ZeroDivisionError):
+        except ZeroDivisionError:
+            print("Raise: Zero denomenator")
             pass
 
-    tank =  float((x / y)) * 100
-    # print(f'z: {tank}')
-    if tank <= 1:
-        return 'E'
-    elif tank >=99:
-        return 'F'
-    else:
-        return str(f'Fuel Available: {tank: .2f}') + '%'
-
+        
 def main():
     output = fuel_monitor()
     print(output)
