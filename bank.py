0d3993710ac8ce0c5522028537c5786d2d4661ad
Deepa <deepav2008@gmail.com>
2025-03-28 13:52:27 +1300

    Add files via upload
    Minor correction to fork statements.

--- bank.py
+++ bank.py
@@ -1,21 +1,34 @@
+""" 
+In a file called bank.py, implement a program that prompts the user for a greeting. 
+If the greeting starts with “hello”, output $0. 
+If the greeting starts with an “h” (but not “hello”), output $20. 
+Otherwise, output $100. 
+Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+ """
+
 def rewardsSystem():
     prompt = input("Greet me! ").strip().lower()
-    if(prompt == "hello"):
-        return "N"
-    elif(prompt.startswith("h")):
-        return "S"
+    
+    if(prompt.startswith("h")):
+        if(prompt == "hello"):
+            return 0
+        else:
+            return 20
     else:
-        return "Y"
+        return 100
 
 def main():
      # imoticons: https://emojipedia.org/
      
     reward = rewardsSystem()
-    if(reward == "N"):
-        print("$0" + "🫨")
-    elif(reward == "S"):
-        print("$20" + "😉")
+    if(reward == 0):
+        # print(isinstance(reward, int))
+        print("$" + reward + "🫨")
+    elif(reward == 20):
+        # print(isinstance(reward, int))
+        print("$" + reward + "😉")
     else:
-        print("$100" + "🤑")
-
+        # print(isinstance(reward, int))
+        print("$s" + reward + "🤑")
+        
 main()
