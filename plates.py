532209bb9c8777d5d06ecd3dca68a13cfe10a068
Deepa <deepav2008@gmail.com>
2025-04-01 11:23:53 +1300

    Add files via upload
    Approach changed to handle the invalid plate numbers as 'ValueError' and exit.

--- plates.py
+++ plates.py
@@ -1,71 +1,68 @@
-def main():
-    plate = input("Plate: ")
-   
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
+""" 
+############################### Vanity Plates #################################
+In Massachusetts, home to Harvard University, it’s possible to request a vanity license plate for your car, with your choice of letters and numbers instead of random ones. Among the requirements, though, are:
 
-# All vanity plates must start with at least two letters.”
-#… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
-# Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
-# No periods, spaces, or punctuation marks are allowed.”
+- All vanity plates must start with at least two letters.
+- … vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.
+- Numbers cannot be used in the middle of a plate; they must come at the end. 
+        For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. 
+-  The first number used cannot be a ‘0’.
+- “No periods, spaces, or punctuation marks are allowed.
+
+In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or 
+Invalid if it does not. Assume that any letters in the user’s input will be uppercase. 
+Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. 
+Assume that s will be a str.
+ """
+import sys
+
+
+def main():
+    prompt = input("Eneter your license plate number: ")
+    output = is_valid(prompt)
+    print(output)
 
 
 def is_valid(s):
-    special_characters = ".!' '"
-    for c in s:
-        if c in special_characters: 
-            print("Invalid character \n")
-            return False
-
-    if 1 < len(s) < 7:
-        prefix = s[0:2]
-        for c in prefix:
-            if c.isdigit():
-                print("Invalid length \n")
-                return False
-            
-        suffix = s[2:len(s)]
-        reversed_str = suffix[:: -1]
-        if suffix.isalnum():
-
-            # Verify that str does not contain number sequence anywhere in the middle of the str
-            for i in range(len(reversed_str)-1):
-                if reversed_str[i].isalpha():
-                    if reversed_str[i+1].isdigit():
-                        print("Invalid numeric substring \n")
-                        return False
-                    
-            # Verify that number sequence does not start with 0 
-            for i in range(len(suffix)):
-                if suffix[i].isalpha():
-                    break
-                else:
-                    # if not non_zero_integer(suffix[i:len(s)]):
-                    if not non_zero_integer(suffix[i]):
-                        print("Invalid number sequence ##1 \n")
-                        return False
-                    else:
-                        return True
-                    
-        elif suffix.isdigit():
-            if not non_zero_integer(suffix[0]):
-                print("Invalid number sequence ##2 \n")
-                return True
-            else:
-                return False
-        
-        else:
-            return True
+    banned_characters = ".!' '"
     
-    else:
-        print("Invalid! Oversize string \n")
-        return False
-        
-def non_zero_integer(n):
-    if int(n[0]) == 0:
-        return False
-    return True
+    try:
 
-main()
+        if (len(s) < 2 or (s[1:2].isalpha() == False)):
+            raise ValueError
+        
+        elif len(s) > 6:
+            raise ValueError
+        
+        
+        elif s.isalpha():
+            return "Valid"
+               
+        else:
+            for c in s:
+                if c in banned_characters:
+                    print("Invalid character: "+ c)
+                    raise ValueError
+                    
+                elif c.isdigit():
+                    if int(c) == 0:
+                        raise ValueError
+                        
+                    else:
+                        x = s.index(c)
+                        suffix = s[x:len(s)]
+
+                        for x in suffix:
+                            if x.isalpha():
+                                raise ValueError
+                            elif x in banned_characters:
+                                raise ValueError                                
+                        return "Valid"                  
+
+    except ValueError:
+        print("Invalid")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
